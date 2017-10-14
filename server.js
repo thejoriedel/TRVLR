2bf0030ee1b963fb75e98f0f440ce958d3b555c4
woojunchoi <emilyhongji@Emily-Kim.local>
2017-10-13 17:00:50 -0700

    almost..almost..

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://thejozhou:database123@ds119585.mlab.com:19585/trvlr');
-mongoose.connection.once('open', () =>{
-  console.log('connected')
-});
+// mongoose.connect('mongodb://thejozhou:database123@ds119585.mlab.com:19585/trvlr');
+// mongoose.connection.once('open', () =>{
+//   console.log('connected')
+// });
 
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
--- src/Threeimgs.js
+++ src/Threeimgs.js
@@ -7,7 +7,7 @@ class Threeimgs extends React.Component {
    return (
    <div id ='lefthalf'>
      <div className='picone' onClick={this.props.toggler}><img className ='imgfirst' src={this.props.firstpics}/><div className ='txtpic'>{this.props.first}</div></div>
-     <div className='pictwo' onClick={this.props.toggler}><img className ='imgfirst' src={this.props.secondpics}/><div className ='txtpic'>{this.props.second}</div></div>
+     <div className='pictwo' onClick={this.props.toggle}><img className ='imgfirst' src={this.props.secondpics}/><div className ='txtpic'>{this.props.second}</div></div>
      <div className='picthree'><p className ='midtext'>{this.props.mention}</p></div>
    </div>
    )
