fa5c777e40e59042441f27fc4aca68f89e70f42b
hcam05 <harry@simpleplots.com>
2017-10-15 20:02:42 -0700

    yelp results now  rendering on screen

---
+++ src/Suggestions.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import YelpResult from './YelpResult';
+
+class Suggestions extends React.Component {
+
+    render() {
+        
+        // return (
+        //     'HI!'
+        // );
+        let yelpSuggs = this.props.data.map(function (x, i) {
+            return <YelpResult data={x} key={i} />
+        });
+        console.log(this.props);
+        return (
+            <div className="yelpResult">
+                <span className="result"> Results </span>
+                {yelpSuggs}
+            </div>
+        );
+    }
+}
+export default Suggestions;
